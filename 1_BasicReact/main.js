8af82219491f92796252ba563ea6e7e90f7e4a22
nguyenmanhcong <cng.buff@gmail.com>
2022-11-05 10:33:27 +0900

    use react and practice with react DOM

---
+++ 1_BasicReact/main.js
@@ -0,0 +1,12 @@
+// // Destructurning : phan ra
+// var course = {
+//     name : 'Jacascript',
+//     price: 1000,
+//     image: "image-address",
+//     children: {
+//         name: "React",
+//     }
+// };
+// var {name:parentName, children:{name:childrenName}} = course;
+// console.log(parentName, childrenName);
+
